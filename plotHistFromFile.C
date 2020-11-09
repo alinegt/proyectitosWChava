723a5959e70ca130f54741c3b9e1ececc5b6ac35
Salvador Ventura <salvadorventurag@gmail.com>
2020-11-08 22:47:05 -1000

    ROOT macro for reading file and plotting histogram

---
+++ plotHistFromFile.C
@@ -0,0 +1,15 @@
+#include "TCanvas.h"
+#include "TROOT.h"
+#include "TTree.h"
+
+void plotHistFromFile(void){
+
+TTree *Tcharge = new TTree("Tcharge","Tcharge");
+Tcharge->ReadFile("processedWaveforms.dat","lineNumber:charge");
+Tcharge->Draw("-charge");
+Tcharge->Print();
+}
+
+int main(){
+    plotHistFromFile();
+}
