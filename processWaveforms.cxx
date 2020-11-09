3a111f06ada05f54d8671e9adf0a47732b468de6
Salvador Ventura <salvadorventurag@gmail.com>
2020-11-09 10:26:51 -1000

    change in variable names

--- processWaveforms.cxx
+++ processWaveforms.cxx
@@ -19,6 +19,11 @@ int main(void)
    float t10=0, t50=0, t90=0, ct10=0, ct50=0, ct90=0;
    float Carga_actual=0, Carga_anterior=0, tiempo=1;
    char c1[10],c2[10],c3[10],c4[10],c5[10];
+   int R=50; // Resistencia en Ohms
+   float sum_ruido1; // Sumatoria en primer parte de la waveform
+   float sum_volt;   // Sumatoria en el rango del pulso
+   float volt;    // Valor de voltaje en osciloscopio
+   float avg_ruido1; // Sumatoria del ruido antes de pulso dividida por el no. de muestras en ese rango
    //Nombre de mi archivo de entrada
    fichero = fopen( "merge.dat", "r" );
    fichero2 = fopen( "merge.dat", "r" );
@@ -29,6 +34,7 @@ int main(void)
    unsigned int ancho=25;        //Ancho del pulso aproximado
    float No_muestras=200;        //Dato puesto durante la adquisición, en nanosegundos
    float No_eventos=10989;       //Dato puesto durante la adquisición, cantidad de eventos tomados
+    int delta_t= 0.5 ;             // delta de tiempo en ns
 
 /************************************************************************************************/
 
@@ -47,32 +53,35 @@ int main(void)
    ruido2=0;
    voltaje_max=-1000;
    voltaje_min=1000; 
+   sum_volt=0;
+   sum_ruido1=0;
+   volt=0;
    for( i=it; i<it+No_muestras; i++ )
    {
-     fscanf( fichero, "%f,%f\n", &x1, &x2);
+     fscanf( fichero, "%f,%f\n", &x1, &volt);
      if (i<it+bin_inicial-1)
      {
-       ruido1=ruido1+x2;
+       sum_ruido1=sum_ruido1+volt;
      }
      if (i>=it+bin_inicial-1 && i<it+(bin_inicial+ancho)-1)
      {
-       if (voltaje_max<x2)
+       if (voltaje_max<volt)
        {
-         voltaje_max=x2;
+         voltaje_max=volt;
        }
-       if (voltaje_min>x2)
+       if (voltaje_min>volt)
        {
-         voltaje_min=x2;
+         voltaje_min=volt;
        }
-       carga=carga+(x2);
+       sum_volt=sum_volt+volt;
      }
      if (i>=it+(bin_inicial + ancho)-1)
      {
-       ruido2=ruido2+x2;
+       ruido2=ruido2+volt;
      }
    }
 //  std::cout<<ruido1<<"     "<<ruido2<<std::endl;
-  ruido1=ruido1/(bin_inicial-1);
+  avg_ruido1=sum_ruido1/(bin_inicial-1);
   ruido2=ruido2/(No_muestras-bin_inicial-ancho+1);
 //  std::cout<<ruido1<<"     "<<ruido2<<std::endl;
 //  if (ruido1<=ruido2)
@@ -86,15 +95,15 @@ int main(void)
   prom_min=prom_min+voltaje_min;
   if (bin_inicial==1)
   {
-     carga=carga;
+     carga=sum_volt;
      voltaje_max=voltaje_max;
      voltaje_min=voltaje_min;
   }
   else
   {
-     carga=carga-(ancho*ruido1);
-     voltaje_max=voltaje_max-ruido1;
-     voltaje_min=voltaje_min-ruido1;
+     carga=sum_volt-(ancho*avg_ruido1);
+     voltaje_max=voltaje_max-avg_ruido1;
+     voltaje_min=voltaje_min-avg_ruido1;
   }
 
   for( iprima=it; iprima<it+No_muestras; iprima++ )
@@ -123,7 +132,7 @@ int main(void)
         tiempo=tiempo+1;
      }
   }
-  outputFile <<i2<<"	"<<carga/50<<"	"<<voltaje_min<<"	"<<voltaje_max<<"	"<<t50-t10<<"	"<<t90-t10<<std::endl;
+  outputFile <<i2<<"	"<<carga/R<<"	"<<voltaje_min<<"	"<<voltaje_max<<"	"<<t50-t10<<"	"<<t90-t10<<std::endl;
   printf( "....................%u\n", i2);
   i2=i2+1;
   it=x1;
