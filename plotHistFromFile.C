060edd677289b900fe3cf4c43a4dd8f333ede648
Salvador Ventura <salvadorventurag@gmail.com>
2020-11-11 20:27:03 -1000

    plotting two histograms

--- plotHistFromFile.C
+++ plotHistFromFile.C
@@ -6,10 +6,31 @@ void plotHistFromFile(void){
 
 TTree *Tcharge = new TTree("Tcharge","Tcharge");
 Tcharge->ReadFile("processedWaveforms.dat","lineNumber:charge");
-Tcharge->Draw("-charge/(1e-12)");
+//Tcharge->Draw("charge");
 Tcharge->Print();
+
+TTree *Tcharge_coil = new TTree("Tcharge_coil","Tcharge_coil");
+Tcharge_coil->ReadFile("processedWaveforms_coil.dat","lineNumber:charge_coil");
+//Tcharge_coil->Draw("charge");
+Tcharge_coil->Print();
+
+TH1F *h1 = new TH1F("h1", "h1 title", 169, 0, 1);
+//h1->Fill;
+//THStack *hs = new THStack("hs","stack example");
+//hs->Add(h1);
+Tcharge->Draw("charge>>h1","","");
+//gPad->GetListOfPrimitives()->Print(); 
+//gPad->GetListOfPrimitives()->At(0);
+//TFrame ((TH1F*)(gPad->GetListOfPrimitives()->At(0)))->SetLineColor(1); 
+Tcharge_coil->Draw("charge_coil","", "same");
+//Tcharge->Draw("charge>>h1");
+//Tcharge_coil->Draw("charge_coil>>h1");
+h1->SetLineColor(2);
 }
 
 int main(){
     plotHistFromFile();
+
 }
+
+
