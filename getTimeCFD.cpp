05b58aef5e85afee8b056318a0eb4c9a837e17d8
Aline Galindo <alinegt@hawaii.edu>
2020-11-15 21:50:16 -1000

    the code getTimeCFD is capable to obtain the canvas of the hitogram of the time difference of signals from channel 1 & 2. The only missing part is the Gaussina fit

--- getTimeCFD.cpp
+++ getTimeCFD.cpp
@@ -19,23 +19,56 @@ time.forLoop();
 ///======================================
 void GetTimeCFD::forLoop()
 {
-    //for (int a_index=0 ; a_index<getNumberOfEntries() ; a_index++) {
-        //m_inputTree->GetEntry(a_index);
-    m_inputTree->GetEntry(0);
-
-        getCFDtime(m_ch1);
-    //s}
+    for (int a_index=0 ; a_index<getNumberOfEntries() ; a_index++)
+    {
+        m_inputTree->GetEntry(a_index);
+        //m_inputTree->GetEntry(0);
+        //cout<<"time CFD: "<<getCFDtime(m_ch1)<<endl;
+        h_timeDiff->Fill(getCFDtime(m_ch1)-getCFDtime(m_ch2));
+    }
+    saveHistogram(h_timeDiff);
 }
 
 
 ///======================================
+
+void GetTimeCFD::saveHistogram(TH1F *a_histogram){
+
+    TCanvas *c = new TCanvas ("c","A3",700,700);
+    const char* a_textFileName = a_histogram->GetName();
+    std::string a_nameFileStr = a_textFileName;
+    a_nameFileStr=a_nameFileStr+".png";
+    const char* a_nameFile = a_nameFileStr.c_str();;
+
+    c->SetGrid();
+
+    //gPad->SetLogz();
+    //gPad->SetLogy();
+    //gPad->SetLogx();
+    //gStyle->SetOptStat(0);
+    gStyle->SetOptFit(1111);
+    //gStyle->SetStatX(0.9);
+    //gStyle->SetStatY(0.9);
+    a_histogram -> GetYaxis() -> SetTitleOffset(1.2);
+    a_histogram -> GetXaxis() -> SetTitleOffset(1.2);
+    a_histogram->Draw();
+    //TPaveText *labeD = new TPaveText(0.5,0.1,0.9,0.2,"brNDC");
+    //labeD->AddText(correlation.c_str());
+    //labeD->Draw();
+    c->Print(a_nameFile);
+    c->cd();
+
+}
+///======================================
 float GetTimeCFD::getCFDtime(std::vector<float> *a_signal){
-    cout<<"here"<<endl;
+    bool a_flag = false;
+    float a_x1{},a_x2{},a_y1{},a_y2{};
     std::vector <float> a_delaySignal;
     std::vector <float> a_resultSignal;
-    int a_delayIndex = 25; //25 samples of delay
+    int a_delayIndex = 5; //25 samples of delay
     float a_fraction = 0.7;
-    cout<<"signal size: "<<a_signal->size();
+    float a_timeCFD{};
+    //cout<<"signal size: "<<a_signal->size();
     ///Negative and delayed signal
     for (long unsigned int a_index=0 ; a_index<a_signal->size()-a_delayIndex;a_index++)
     {
@@ -43,24 +76,51 @@ float GetTimeCFD::getCFDtime(std::vector<float> *a_signal){
 
     }
 
-    cout<<"a_delay size: "<<a_delaySignal.size();
+    //cout<<"a_delay size: "<<a_delaySignal.size();
     ///Sum of the signals
     for (long unsigned int a_index=0 ; a_index<a_signal->size()-a_delayIndex;a_index++)
     {
-        cout<<a_index<<" "<<a_signal->at(a_index)<<" "<<-a_signal->at(a_index+a_delayIndex)<<endl;
+        //cout<<a_index<<" "<<a_signal->at(a_index)<<" "<<-a_signal->at(a_index+a_delayIndex)<<endl;
         a_resultSignal.push_back(a_fraction *a_signal->at(a_index)+a_delaySignal.at(a_index));
     }
-    cout<<"Result signal"<<endl;
+
+
 
     for(long unsigned int a_index=0 ; a_index<a_resultSignal.size();a_index++)
     {
+        if ((a_resultSignal.at(a_index)<-100.)&&(a_flag==false))
+        {a_flag = true;}
+        if ((a_resultSignal.at(a_index)>0)&&(a_flag==true)){
+            //cout<<a_index<<" "<<a_resultSignal.at(a_index)<<endl;
+                a_x1 = (float)a_index-1;
+                a_x2 = (float)a_index;
+                a_y1 = a_resultSignal.at(a_index-1);
+                a_y2 = a_resultSignal.at(a_index);
+                a_flag = false;
 
-        cout<<a_index<<" "<<a_resultSignal.at(a_index)<<endl;
+                a_timeCFD = getInterpolationX(a_x1,a_x2,a_y1,a_y2);
+                //cout<<a_x1<<" "<<a_x2<<" "<<a_y1<<" "<<a_y2<<endl;
+        }
+
+
+        //cout<<a_index<<" "<<a_resultSignal.at(a_index)<<endl;
     }
-    return 0;
+    //cout<<"time CFD: "<<a_timeCFD<<endl;
+    return a_timeCFD;
 }
 ///======================================
+/// Zero crossing (y=0)
+float GetTimeCFD::getInterpolationX(float a_x1, float a_x2, float a_y1, float a_y2)
+{
+    float a_interpolationXNum{};
+    float a_slope{};
+    a_slope = (a_y2-a_y1)/(a_x2-a_x1);
+    a_interpolationXNum = a_x1-(a_y1/a_slope);
 
+    return a_interpolationXNum;
+}
+
+///======================================
 bool GetTimeCFD::loadDataFile(const std::string & a_inputFile)
 {
     string inFile = a_inputFile;
--- getTimeCFD.h
+++ getTimeCFD.h
@@ -33,11 +33,16 @@ public:
     Long64_t getNumberOfEntries();
     void forLoop();
     float getCFDtime(std::vector<float> *a_signal);
+    float getInterpolationX(float a_x1, float a_x2, float a_y1, float a_y2);
+    void saveHistogram(TH1F *a_histogram);
 private:
     TTree* m_inputTree;
     std::vector<float> *m_ch1=0;
     std::vector<float> *m_ch2=0;
     bool m_fileLoaded;
+    //parameters of the histogram must be changed in order to set a proper range
+    TH1F *h_timeDiff = new TH1F("h_timeDiff", "h_timeDiff", 70, 435, 440);
+
 };
 
 #endif
