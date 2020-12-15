2dbad0527c5c9d5eb80f7d7a63cf116f44813021
Salvador Ventura <salvadorventurag@gmail.com>
2020-12-14 18:29:06 -1000

    setting 2 modes, for timing res and for single ch

--- CFD/getTimeCFD.cpp
+++ CFD/getTimeCFD.cpp
@@ -15,12 +15,14 @@ GetTimeCFD time;
 time.loadDataFile(inputFile);
 cout<<"The tree has: "<<time.getNumberOfEntries()<<" events"<<endl;
 
-time.saveFile(outputFile);
-time.forLoop();
+time.setOutFile(outputFile);
+//time.loopOverEntriesTimingRes();
+time.loopOverEntries();
+
 }
 ///*********************************************************************
 ///======================================
-void GetTimeCFD::forLoop()
+void GetTimeCFD::loopOverEntriesTimingRes()
 {
     float a_timeCh1{}, a_timeCh2{};
     for (int a_index=0 ; a_index<getNumberOfEntries() ; a_index++)
@@ -28,10 +30,10 @@ void GetTimeCFD::forLoop()
         m_inputTree->GetEntry(a_index);
         //m_inputTree->GetEntry(0);
         //cout<<"time CFD: "<<getCFDtime(m_ch1)<<endl;
-        a_timeCh1=getCFDtime(m_ch1, .8, 18,1);
+        a_timeCh1=getCFDtimeTimingRes(m_ch1, .8, 18,1);
         //  m_resultSignal.clear();
         // m_delaySignal.clear();getInterpolationX
-        a_timeCh2=getCFDtime(m_ch2, .8, 18, 2);
+        a_timeCh2=getCFDtimeTimingRes(m_ch2, .8, 18, 2);
 
 
         h_timeDiff->Fill(a_timeCh1-a_timeCh2);
@@ -54,6 +56,48 @@ void GetTimeCFD::forLoop()
 
 }
 
+///*********************************************************************
+///======================================
+void GetTimeCFD::loopOverEntries()
+{
+    float a_timeCh1{}, a_timeCh2{};
+    
+    m_inputTree->Draw("noise>>h_noise", "", "goff");
+    TH1F *h_noise = (TH1F *)gDirectory->Get("h_noise");
+    float noise_mean = h_noise->GetMean();
+
+    for (int a_index=0 ; a_index<getNumberOfEntries() ; a_index++)
+    {
+        m_inputTree->GetEntry(a_index);
+        auto minimum=std::min_element(  m_ch1->begin(), m_ch1->end() ) ;
+        //m_inputTree->GetEntry(0);
+        //cout<<"time CFD: "<<getCFDtime(m_ch1)<<endl;
+        if (  *minimum  <noise_mean*(-1)  )  {
+        a_timeCh1=getCFDtime(m_ch1, .5, 30);
+        //  m_resultSignal.clear();
+        // m_delaySignal.clear();getInterpolationX
+        // a_timeCh2=getCFDtime(m_ch2, .8, 18, 2);
+
+       
+        // h_timeDiff->Fill(a_timeCh1-a_timeCh2);
+        // m_timeDifference = a_timeCh1-a_timeCh2;
+        m_timeCh1=a_timeCh1;
+        // m_timeCh2=a_timeCh2;
+
+        m_outputTree->Fill();
+        m_resultSignal_ch1.clear();
+        m_delaySignal_ch1.clear();
+        // m_resultSignal_ch2.clear();
+        // m_delaySignal_ch2.clear();
+        }
+    }
+    m_outputFile->Write();
+    // saveHistogram(h_timeDiff);
+    std::cout<<"File written"<<std::endl;
+    m_outputFile->Close();
+
+
+}
 
 ///======================================
 
@@ -85,7 +129,7 @@ void GetTimeCFD::saveHistogram(TH1F *a_histogram){
 
 }
 ///======================================
-float GetTimeCFD::getCFDtime(std::vector<float> *a_signal, float a_fraction, float a_delay, int ch){
+float GetTimeCFD::getCFDtimeTimingRes(std::vector<float> *a_signal, float a_fraction, float a_delay, int ch){
     std::vector <float> a_delaySignal;
     std::vector <float> a_resultSignal;    
     bool a_flag = false;
@@ -146,6 +190,71 @@ float GetTimeCFD::getCFDtime(std::vector<float> *a_signal, float a_fraction, flo
     return a_timeCFD;
 }
 ///======================================
+float GetTimeCFD::getCFDtime(std::vector<float> *a_signal, float a_fraction, float a_delay){
+    std::vector <float> a_delaySignal;
+    std::vector <float> a_resultSignal;    
+    bool a_flag = false;
+    float a_x1{},a_x2{},a_y1{},a_y2{};
+    float a_timeCFD{};
+    //cout<<"signal size: "<<a_signal->size();
+    ///Negative and delayed signal
+    for (long unsigned int a_index=0 ; a_index<a_signal->size();a_index++)
+    {
+        a_delaySignal.push_back(a_signal->at(a_index)*(-1));
+
+    }
+    if (a_delay>0){
+    std::rotate(a_delaySignal.begin(), a_delaySignal.begin()+a_delay,a_delaySignal.end());
+    }
+    else{
+    std::rotate(a_delaySignal.begin(), a_delaySignal.begin()+ a_delaySignal.size()+a_delay,a_delaySignal.end());
+
+    }
+    //cout<<"a_delay size: "<<a_delaySignal.size();
+    ///Sum of the signals
+    for (long unsigned int a_index=0 ; a_index<a_signal->size();a_index++)
+    {
+        a_resultSignal.push_back(a_fraction *a_signal->at(a_index)+a_delaySignal.at(a_index));
+    }
+    // std::rotate(a_delaySignal.begin(), a_delaySignal.begin()+a_fraction,a_delaySignal.end())
+
+    for(long unsigned int a_index=0 ; a_index<a_resultSignal.size();a_index++)
+    {
+        if ((a_resultSignal.at(a_index)>0.03)&&(a_flag==false))
+        {a_flag = true;}
+        if ((a_resultSignal.at(a_index)<0)&&(a_flag==true)){
+            //cout<<a_index<<" "<<a_resultSignal.at(a_index)<<endl;
+                a_x1 = (float)a_index-1;
+                a_x2 = (float)a_index;
+                a_y1 = a_resultSignal.at(a_index-1);
+                a_y2 = a_resultSignal.at(a_index);
+                a_flag = false;
+
+                a_timeCFD = getInterpolationX(a_x1,a_x2,a_y1,a_y2);
+                //cout<<a_x1<<" "<<a_x2<<" "<<a_y1<<" "<<a_y2<<endl;
+        }
+
+
+        //cout<<a_index<<" "<<m_resultSignal.at(a_index)<<endl;
+    }
+    // if (ch==1) {
+        m_resultSignal_ch1= a_resultSignal;
+        m_delaySignal_ch1=a_delaySignal;
+    // } else
+    // {
+    //     m_resultSignal_ch2=a_resultSignal;
+    //     m_delaySignal_ch2=a_delaySignal;
+    // }
+    
+    
+    //cout<<"time CFD: "<<a_timeCFD<<endl;
+    return a_timeCFD;
+}
+
+
+
+
+///======================================
 /// Zero crossing (y=0)
 float GetTimeCFD::getInterpolationX(float a_x1, float a_x2, float a_y1, float a_y2)
 {
@@ -163,8 +272,8 @@ bool GetTimeCFD::loadDataFile(const std::string & a_inputFile)
     string inFile = a_inputFile;
     auto f = new TFile(inFile.c_str(),"READONLY");
     m_inputTree = (TTree*)f->Get("T");
-    m_inputTree -> SetBranchAddress("ch1",&m_ch1);
-    m_inputTree -> SetBranchAddress("ch2",&m_ch2);
+    m_inputTree -> SetBranchAddress("voltage",&m_ch1);
+   // m_inputTree -> SetBranchAddress("ch2",&m_ch2);
     m_fileLoaded = true;
     return m_fileLoaded;
 }
@@ -178,7 +287,7 @@ Long64_t GetTimeCFD::getNumberOfEntries()
 }
 ///======================================
 /// Save Tree root in a output file
-void GetTimeCFD::saveFile(const std::string & a_outputFile){
+void GetTimeCFD::setOutFile(const std::string & a_outputFile){
 
     string outFile = a_outputFile;
     m_outputFile = new TFile(outFile.c_str(), "RECREATE");
