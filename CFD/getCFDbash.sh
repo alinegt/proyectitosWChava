03a7d8f92e15081a3441bc22da1a9d2cfedbc4a7
Salvador Ventura <salvadorventurag@gmail.com>
2020-12-24 20:50:38 -1000

    bash to get CFD from root files

--- CFD/getCFDbash.sh
+++ CFD/getCFDbash.sh
@@ -1,5 +1,31 @@
 #! /bin/bash
-
-inputFile="/home/salvador/github/proyectitosWChava/SPE/data/processed/noCoil.root"
 make
-./getTimeCFD $inputFile cfdResults.root     
+execPath=/home/salvador/github/proyectitosWChava/CFD
+inputPath=/home/salvador/github/proyectitosWChava/SPE/data/processed/
+outputPath=/home/salvador/github/proyectitosWChava/CFD/data/rootFiles/
+fileExt=".root"
+
+make
+cd $inputPath
+
+for file in *
+do
+#inputPath=/home/salvador/github/proyectitosWChava/SPE/data/processed/
+outputPath=/home/salvador/github/proyectitosWChava/CFD/data/rootFiles/
+
+processedPath="${inputPath}""${file}"
+nameOnly="${file##*/}"
+nameOnly="${file%.*}"
+outputPath="${outputPath}""${nameOnly}""${fileExt}"
+echo $processedPath
+echo $outputPath
+cd $execPath
+./getTimeCFD $processedPath $outputPath 
+cd $inputPath
+#processedPath=""
+#outputPath=""
+done
+#cd ${outputPath}
+
+# rm SPEmontage.png
+# montage -geometry 1000x1000+2+2  *.png SPEmontage.png
