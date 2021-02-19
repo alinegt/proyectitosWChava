10cfd38f0ff0f04e339261a7e15651b3e634042a
Salvador Ventura <salvadorventurag@gmail.com>
2021-02-18 22:02:46 -1000

    Replace explicit path by PWD, clean dirs

--- SPE/getParamRecursive.sh
+++ SPE/getParamRecursive.sh
@@ -9,27 +9,30 @@
 # The output are the plots and a file with the extracted SPE parameters
 # using the getSPEparam.C root macro.
 make -B
-execPath=/home/salvador/github/proyectitosWChava/SPE
-inputPath=/home/salvador/github/proyectitosWChava/SPE/data/processed/
+localPath=$PWD
+execPath=$localPath
+inputPath=$localPath/data/processed/
 outputFileName=SPEparams
-mergePath=/home/salvador/github/proyectitosWChava/SPE/data/merge/
+mergePath=$localPath/data/merge/
 
-rm /home/salvador/github/proyectitosWChava/SPE/data/SPEparam/SPEparams.dat
+rm $localPath/data/occupancy/*.png
+rm $localPath/data/plots/*.png
+rm $localPath/data/timePlots/*.png
+rm $localPath/data/SPEparam/SPEparams.dat
+
 cd $inputPath
 for file in *
 do
 processedPath="${inputPath}""${file}"
-# mergeFile="${mergePath}""${file%.*}""_merge.dat"
 echo $mergeFile
 echo $processedPath
 cd $execPath
 ./SPE_and_timing $processedPath $outputFileName 
 cd $inputPath
 done
-cd /home/salvador/github/proyectitosWChava/SPE/data/plots/
-rm SPEmontage.png
+
+cd $localPath/data/plots/
 montage -geometry 1000x1000+2+2  *.png SPEmontage.png
 
-cd /home/salvador/github/proyectitosWChava/SPE/data/timePlots/
-rm Timemontage.png
+cd $localPath/data/timePlots/
 montage -geometry 1000x1000+2+2 *.png Timemontage.png
