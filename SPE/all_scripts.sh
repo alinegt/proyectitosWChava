83ebb34b263e4c01275b3c8fb6f5ace6767bea46
Salvador Ventura <salvadorventurag@gmail.com>
2021-02-28 16:42:57 -1000

    Pass name parameter for second Directory to process

--- SPE/all_scripts.sh
+++ SPE/all_scripts.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
-noiseMaxIndex=$1
-./merge_and_process.sh  /home/salvador/Documents/WPT/Zscan_2021/alumBox $noiseMaxIndex  #/home/salvador/Documents/WPT/data_feb8_2021/toPresent/Nocoil  # /home/salvador/Documents/WPT/noise
-./getParamRecursive.sh alumBox $noiseMaxIndex 
+name=$1
+noiseMaxIndex=$2
+
+./merge_and_process.sh  /home/salvador/Documents/WPT/Zscan_2021/$name $noiseMaxIndex  #/home/salvador/Documents/WPT/data_feb8_2021/toPresent/Nocoil  # /home/salvador/Documents/WPT/noise
+./getParamRecursive.sh $name $noiseMaxIndex 
+
+
+name2=$3
+
+./merge_and_process.sh  /home/salvador/Documents/WPT/Zscan_2021/$name2 $noiseMaxIndex  #/home/salvador/Documents/WPT/data_feb8_2021/toPresent/Nocoil  # /home/salvador/Documents/WPT/noise
+./getParamRecursive.sh $name2 $noiseMaxIndex 
+
+
 # ./merge_and_process.sh  /home/salvador/Documents/WPT/Zscan_2021/alumBox
 # ./getParamRecursive.sh
