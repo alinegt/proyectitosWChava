a3e68c361bda7829321039395daca33e4e5ef0df
sventurag <salvadorventurag@gmail.com>
2021-06-16 14:16:39 -1000

    Add run name in command line

--- timing/getCFDbash.sh
+++ timing/getCFDbash.sh
@@ -1,19 +1,37 @@
 #! /bin/bash
+
+function createDir(){
+
+if [[ -d "$1" ]]; then
+    echo "$1 exists on your filesystem."
+    rm $1/*
+else 
+    echo "$1 does not exist on your filesystem."
+    echo Creating $1 directory
+    mkdir $1
+fi
+
+} 
+
 runName=$1
 execPath=/home/idlab-52/github/proyectitosWChava/timing
 inputPath=/home/idlab-52/github/proyectitosWChava/SPE/data/cutRootFiles/$runName/
-outputPath=/home/idlab-52/github/proyectitosWChava/timing/data/rootFiles/
+outputPath=/home/idlab-52/github/proyectitosWChava/timing/data/rootFiles/$runName/
+plotsPath=/home/idlab-52/github/proyectitosWChava/timing/data/plots/$runName/
+
 fileExt=".root"
 make -B
-cd "$execPath""/data""/plots/"
-rm *
+# cd "$execPath""/data""/plots/"
+
+createDir $plotsPath
+createDir $outputPath
 
 cd $inputPath
 
 for file in *
 do
 #inputPath=/home/idlab-52/github/proyectitosWChava/SPE/data/processed/
-outputPath=/home/idlab-52/github/proyectitosWChava/timing/data/rootFiles/
+# outputPath=/home/idlab-52/github/proyectitosWChava/timing/data/rootFiles/
 
 processedPath="${inputPath}""${file}"
 nameOnly="${file##*/}"
@@ -22,12 +40,12 @@ outputPath="${outputPath}""${nameOnly}""${fileExt}"
 echo $processedPath
 echo $outputPath
 cd $execPath
-./getTimeCFD $processedPath $outputPath 
+./getTimeCFD  $processedPath $outputPath  $1
 cd $inputPath
-#processedPath=""
-#outputPath=""
+processedPath=""
+outputPath=""
 done
-cd "$execPath""/data""/plots/"
+cd $plotsPath
 montage -geometry +2+2  *.png "$1""montage.png"
 
 # cd $execPath
